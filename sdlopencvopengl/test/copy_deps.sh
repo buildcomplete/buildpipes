43d9d7998e5eb79051a08e96e39c5796b63f0c9c
Christian Gleerup <buildcomplete@users.noreply.github.com>
2025-03-09 23:24:03 +0100

    Cmake and copy file to copy dependencies

---
+++ sdlopencvopengl/test/copy_deps.sh
@@ -0,0 +1,33 @@
+cd build
+make
+cd ..
+cp ./build/hello_world ./dist/hello_world
+cd dist
+cp /usr/local/lib/libSDL3.so.0 .
+cp /lib/x86_64-linux-gnu/libopencv_imgcodecs.so.4.2 .
+cp /lib/x86_64-linux-gnu/libopencv_core.so.4.2 .
+cp /lib/x86_64-linux-gnu/libopencv_imgproc.so.4.2 .
+cp /lib/x86_64-linux-gnu/libwebp.so.6 .
+cp /lib/x86_64-linux-gnu/libtiff.so.5 .
+cp /lib/x86_64-linux-gnu/libIlmImf-2_3.so.24 .
+cp /lib/libgdal.so.26 .
+cp /lib/x86_64-linux-gnu/libtbb.so.2 .
+cp /lib/x86_64-linux-gnu/libHalf.so.24 .
+cp /lib/x86_64-linux-gnu/libIex-2_3.so.24 .
+cp /lib/x86_64-linux-gnu/libIlmThread-2_3.so.24 .
+cp /lib/libarmadillo.so.9 .
+cp /lib/x86_64-linux-gnu/libpoppler.so.97 .
+cp /lib/x86_64-linux-gnu/libjson-c.so.4 .
+cp /lib/x86_64-linux-gnu/libqhull.so.7 .
+cp /lib/x86_64-linux-gnu/libepsilon.so.1 .
+cp /lib/x86_64-linux-gnu/libnetcdf.so.15 .
+cp /lib/x86_64-linux-gnu/libCharLS.so.2 .
+cp /lib/x86_64-linux-gnu/libcfitsio.so.8 .
+cp /lib/x86_64-linux-gnu/libproj.so.15 .
+cp /lib/x86_64-linux-gnu/libdapclient.so.6 .
+cp /lib/x86_64-linux-gnu/libdap.so.25 .
+cp /lib/x86_64-linux-gnu/libspatialite.so.7 .
+cp /lib/x86_64-linux-gnu/libpcre.so.3 .
+cp /lib/x86_64-linux-gnu/libcrypto.so.1.1 .
+cp /lib/x86_64-linux-gnu/libsuperlu.so.5 .
+cd ..
